8e5c2caa394a386cd42ea9c84054b4a9254a3ee6
DwaraknathT <dwarakasharma@gmail.com>
2020-09-25 10:38:27 +0530

    test args

--- src/utils/args.py
+++ src/utils/args.py
@@ -9,7 +9,7 @@ parser.add_argument(
 parser.add_argument(
   '--seed', default=1, type=int, help='random seed')
 parser.add_argument(
-  '--model', default='train', type=bool, help='In eval mode or not')
+  '--mode', default='train', type=bool, help='In eval mode or not')
 parser.add_argument(
   '--load_prev', default=False, type=bool, help='Load prev weights')
 
