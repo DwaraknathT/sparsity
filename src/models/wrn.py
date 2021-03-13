4596eaa4c0c5f84407c284760861bc84e58b8153
DwaraknathT <dwarakasharma@gmail.com>
2021-03-13 17:14:00 +0530

    wrn layer import fox

--- src/models/wrn.py
+++ src/models/wrn.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 
-from models.layers import MaskedConv
+from src.models.layers import MaskedConv
 from src.models.registry import register
 
 
